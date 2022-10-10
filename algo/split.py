329296443066a1f24aa16d4a2564a211a55e7bf9
范皓年 <fhn037@126.com>
2022-10-10 20:04:47 +0800

    add algo.split.synthesis

--- algo/split.py
+++ algo/split.py
@@ -5,3 +5,8 @@ def interpolation(data: pd.DataFrame):
     data = data.set_index('real_timestamp').loc[:, 'amp0':'phs63'].resample('0.001S').interpolate(method='polynomial', order=2).reset_index()
     data.loc[:, 'real_timestamp'] = data['real_timestamp'].apply(lambda x: x.total_seconds())
     return data.dropna()
+
+def synthesis(data: pd.DataFrame):
+    data["amp"] = data.loc[:,'amp0':'amp63'].apply(lambda x: x.mean(), axis=1)
+    data["phs"] = data.loc[:,'phs0':'phs63'].apply(lambda x: x.mean(), axis=1)
+    return data[['real_timestamp', 'amp', 'phs']]
