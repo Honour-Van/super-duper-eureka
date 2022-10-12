176b37829cc3e39232aa5a370624f03f8235bba1
范皓年 <fhn037@126.com>
2022-10-12 15:32:29 +0800

    Update draw_one

--- utils/draw_signal.py
+++ utils/draw_signal.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from math import ceil
 
-def draw_one(data: pd.DataFrame, target='phs20', s=0, e=-1, color_range=[]):
+def draw_one(data: pd.DataFrame, target='phs20', s=0, e=float('inf'), color_range=[]):
     # 用于绘制某个确定子载波的图样，是查看功能
     # color_range是给后续警告留出的接口，注意传入list前小后大
-    sns.lineplot(data=data[s:e], x='real_timestamp', y=target)
+    sns.lineplot(data=data[(data['real_timestamp']>s)&(data['real_timestamp']<e)], x='real_timestamp', y=target)
     for r in color_range:
         plt.axvspan(xmin=r[0], xmax=r[1], color='red', alpha=0.3)
     plt.show()
