81f8bd66d20a2b265630f70baa3a2c83b2b19a2c
范皓年 <fhn037@126.com>
2022-10-12 17:40:18 +0800

    add algo.split.filter

--- algo/split.py
+++ algo/split.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from scipy import signal
 
 def interpolation(data: pd.DataFrame):
     data.loc[:, 'real_timestamp'] = data['real_timestamp'].apply(pd.Timedelta, args=("S"))
@@ -10,3 +11,9 @@ def synthesis(data: pd.DataFrame):
     data["amp"] = data.loc[:,'amp0':'amp63'].apply(lambda x: x.mean(), axis=1)
     data["phs"] = data.loc[:,'phs0':'phs63'].apply(lambda x: x.mean(), axis=1)
     return data[['real_timestamp', 'amp', 'phs']]
+
+def filter(data: pd.DataFrame, filt_col=['amp', 'phs'], kernel_size=1001):
+    pd.options.mode.chained_assignment = None  # default='warn'
+    data['filtered_phs'] = signal.medfilt(data['phs'], kernel_size=kernel_size)
+    data['filtered_amp'] = signal.medfilt(data['amp'], kernel_size=kernel_size)
+    return data
